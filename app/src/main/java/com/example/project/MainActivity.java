8e81968a20213dea6f92bb13b0ac3056e374e9b9
b20isasa <b20isasa@student.his.se>
2023-05-10 08:29:51 +0200

    Lagt till knapp och andra sida.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,33 +1,52 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.filament.View;
 
 public class MainActivity extends AppCompatActivity {
-private RecyclerView recyclerView;
- Button button_main;
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+    private RecyclerView recyclerView;
+     Button button_main;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        Button b = findViewById(R.id.button_main);
-        button_main =findViewById(R.id.button_main);
-
-
+        recyclerView = findViewById(R.id.recyclerView);
+        button_main = findViewById(R.id.button_main);
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+        button_main.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, MainActivity2.class));
+        });
 
     }
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TextView prefTextRef = new TextView(this);
+        prefTextRef = (TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+    }
 }
+
+
 
 /* @Override
         public void OnClick(View view){
--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -3,18 +3,52 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-private TextView textView;
-Button button_main;
+    Button button2;
+    EditText editView;
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        TextView textView = findViewById(R.id.textView);
+
+        editView = findViewById(R.id.editView);
+        TextView prefTextRef = findViewById(R.id.prefText);
+        button2 = findViewById(R.id.button2);
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TextView prefTextRef;
+                String prefString = editView.getText().toString();
+                myPreferenceEditor.putString("MyAppPreferenceString", prefString);
+                myPreferenceEditor.apply();
+                finish();
+            }
+        });
 
     }
 }
+/*
+button2.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity2.this, MainActivity.class));
+        });
+
+        TextView prefTextRef;
+        TextView prefTextRef = new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+
+ */

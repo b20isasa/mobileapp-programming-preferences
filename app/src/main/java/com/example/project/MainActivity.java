c8b5cd8d07aebba3dc08fa4047abb510c1120237
b20isasa <b20isasa@student.his.se>
2023-05-12 15:04:12 +0200

    fix

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -7,18 +7,19 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import com.google.android.filament.View;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences myPreferenceRef;
     private SharedPreferences.Editor myPreferenceEditor;
     private RecyclerView recyclerView;
     private TextView textView;
-    Button button_main;
+    Button button;
 
 
     @Override
@@ -27,16 +28,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        myPreferenceRef = getSharedPreferences("MypreferencesRef", MODE_PRIVATE);
+
         textView = findViewById(R.id.textView);
+        myPreferenceRef = getSharedPreferences("preferences", MODE_PRIVATE);
         recyclerView = findViewById(R.id.recyclerView);
-        button_main = findViewById(R.id.button_main);
-
-
-        button_main.setOnClickListener(view -> {
-            startActivity(new Intent(MainActivity.this, MainActivity2.class));
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new android.view.View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "tillbaka");
+                Intent intent = new Intent (MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
         });
-
     }
 
     @Override
@@ -48,13 +52,3 @@ public class MainActivity extends AppCompatActivity {
 }
 
 
-/*
- prefTextRef=(TextView)findViewById(R.id.prefText);
-        prefTextRef.setText(myPreferenceRef.getString("namn", "No preference found."));
-        //textView.setText(namn);
-  myPreferenceRef = getPreferences(MODE_PRIVATE);
-        myPreferenceEditor = myPreferenceRef.edit();
-@Override
-        public void OnClick(View view){
-            startActivity(new Intent((MainActivity.this,MainActivity2.class));
-        }*/
--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -1,7 +1,6 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,35 +9,38 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
     Button button2;
     EditText name;
     private SharedPreferences myPreferenceRef;
-
     Button knapp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-       // myPreferenceEditor = myPreferenceRef.edit();
-       //TextView prefTextRef = findViewById(R.id.prefText);
-        name = findViewById(R.id.editView);
-        myPreferenceRef = getSharedPreferences("MypreferencesRef", MODE_PRIVATE);
-
+        SharedPreferences.Editor myPreferenceEditor = myPreferenceRef.edit();
         button2 = findViewById(R.id.button2);
-        button2.setOnClickListener((v)-> {finish();} );
-        //myPreferenceRef = getPreferences(MODE_PRIVATE);
-
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "Back button pressed");
+                Intent intent = new Intent (MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        name = findViewById(R.id.name);
         knapp = findViewById(R.id.knapp);
-        knapp.setOnClickListener((v)->  {
+        myPreferenceRef = getSharedPreferences("preferences", MODE_PRIVATE);
+        knapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 SharedPreferences.Editor editor = myPreferenceRef.edit();
-              //  String prefString = editView.getText().toString();
                 editor.putString("name", name.getText().toString());
-               // prefTextRef.setText(myPreferenceRef.getString("name", "No preference found."));
                 editor.apply();
+            }
         });
     }
 }
